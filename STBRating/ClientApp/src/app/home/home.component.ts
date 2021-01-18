738caca67d7dbf74e4bb38aed8678012b0eda4f3
iamnga <ngann250195@gmail.com>
2021-01-18 16:54:40 +0700

    Add UI

--- STBRating/ClientApp/src/app/home/home.component.ts
+++ STBRating/ClientApp/src/app/home/home.component.ts
@@ -3,6 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.scss']
 })
 export class HomeComponent {
+  max: number = 5;
+  rate: number = 0;
+  isShowOtherReason = false;
+  isSuccess = false;
+
+  rating() {
+    setTimeout(() => {
+      console.log(this.rate);
+    }, 200)
+  }
+
+  showOtherReason() {
+    this.isShowOtherReason = !this.isShowOtherReason;
+  }
+
+  submit() {
+    this.isSuccess = true;
+  }
 }
